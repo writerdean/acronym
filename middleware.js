bbbb56d23f126dd0f5e9bd98d3e21566f352a0cc
Tracy Dean <tracyblackadder@yahoo.com>
2017-04-09 12:46:17 +1000

    added middleware and server files

---
+++ middleware.js
@@ -0,0 +1,12 @@
+var iddleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.original)
+		next();
+	}
+};
+
+module.exports = middleware; 
